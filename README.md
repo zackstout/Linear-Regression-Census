cff7f7569b520ad827f753661b46891ab79511a6
Zack Stout <zackstout@gmail.com>
2018-03-25 22:03:47 -0500

    now have correlation for each column against each other column

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Census Data
 Learning linear regression by exploring this census data from [Data.gov](https://catalog.data.gov/dataset/2010-census-populations-by-zip-code). The code is pretty ugly right now -- certainly not pythonic -- but I believe I've transcribed the logic of linear regression correctly.
+
+Big thanks to statisticsfun for creating the [video](https://www.youtube.com/watch?v=w2FKXOa0HGA) that made this click for me!
--- index.py
+++ index.py
@@ -3,7 +3,6 @@
 # Automate it to check correlations for each of its (options.len)*(options.len - 1)/2 possibilities.
 # The /2 because it's perfecly symmetric between x and y.
 
-
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib import style
@@ -24,13 +23,12 @@ denominators = []
 
 nums2 = []
 denoms2 = []
+currentX = ''
+currentY = ''
 # print(df.head(20))
 
 # Array to hold all totals: {x: '', y: '', correlation: ''}
 totals = []
-
-
-# First step will be figuring out all possible responses and converting them into numbers.
 
 # print(df['Average Household Size'])
 
@@ -46,11 +44,7 @@ options = ['Zip Code', 'Total Population', 'Median Age', 'Total Males', 'Total F
 #     numberHouses.append(num)
 
 
-
-
-# print(sizes)
 def getCorrelation():
-
     # Well it's pretty ugly code, but here's linear regression (I think):
     avgSize = sum(sizes)/len(sizes)
     avgNum = sum(numberHouses)/len(numberHouses)
@@ -84,6 +78,9 @@ def getCorrelation():
     r_squared = sum(nums2)/sum(denoms2)
 
     print("CORRELATION: ", r_squared)
+    print()
+
+    return r_squared
 
 
 # Interesting: turns out there's *no hoisting* in Python:
@@ -106,7 +103,23 @@ for i, opt in enumerate(options):
             sizes.append(x)
         for y in df[options[i+1]]:
             numberHouses.append(y)
-        getCorrelation()
+
+        r_squared = getCorrelation()
+        total = {
+            'r_squared': r_squared,
+            'X': opt,
+            'Y': options[i+1]
+        }
+        totals.append(total)
+
+
+
+
+print(totals)
+
+
+
+
 
 
 
